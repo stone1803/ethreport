a0e3bc69d25594839190c4ff24e5d6c7b60f1927
Phuc Le <phucle@Phucs-MacBook-Pro.local>
2020-02-18 22:14:02 +0700

    oke

--- src/Layout/Ethmap.js
+++ src/Layout/Ethmap.js
@@ -2,39 +2,65 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class Ethmap extends Component {
-  renderWokers = ()=>{
-    let {workers }= this.props;
-    return workers.map((item,index)=>{
-      return <div className="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-      <div className="card card-stats">
-        <div className="card-header card-header-success card-header-icon">
-          <div className="card-icon">
-            <i className="material-icons">payment</i>
+  renderWokers = () => {
+    let { workers } = this.props;
+    return workers.map((item, index) => {
+      if((item.currentHashrate / 100000).toFixed(0)<100){
+        return (
+          <div className="col-xl-3 col-lg-6 col-md-6 col-sm-6">
+            <div className="card card-stats">
+              <div className="card-header card-header-danger card-header-icon">
+                <div className="card-icon">
+                  <i className="material-icons">payment</i>
+                </div>
+                <p className="card-category">{item.worker}</p>
+                <h3 className="card-title">
+                  {(item.currentHashrate / 1000000).toFixed(0)} / MHZ
+                </h3>
+              </div>
+              <div className="card-footer">
+                <div className="stats">
+                  <i className="material-icons">date_range</i> Last 24 Hours
+                </div>
+              </div>
+            </div>
           </div>
-    <p className="card-category">{item.worker}</p>
-          <h3 className="card-title">
-            {((item.currentHashrate)/1000000).toFixed(0)} / MHZ
-          </h3>
-        </div>
-        <div className="card-footer">
-          <div className="stats">
-            <i className="material-icons">date_range</i> Last 24 Hours
+        );
+      }else{
+        return (
+          <div className="col-xl-3 col-lg-6 col-md-6 col-sm-6">
+            <div className="card card-stats">
+              <div className="card-header card-header-success card-header-icon">
+                <div className="card-icon">
+                  <i className="material-icons">payment</i>
+                </div>
+                <p className="card-category">{item.worker}</p>
+                <h3 className="card-title">
+                  {(item.currentHashrate / 1000000).toFixed(0)} / MHZ
+                </h3>
+              </div>
+              <div className="card-footer">
+                <div className="stats">
+                  <i className="material-icons">date_range</i> Last 24 Hours
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
-    })
-  }
+        );      
+      }
+    });
+  };
   render() {
-    
     let { workers } = this.props;
     console.log(workers);
     return (
-      <div className="row container">
-        {this.renderWokers()}
+      <div>
+        <p className="text-center">
+          Màu đỏ là máy đã tắt vui lòng chọn thông báo để báo cho quản trị{" "}
+        </p>
+        <div className="row container">{this.renderWokers()}</div>
       </div>
-    
-  );
+    );
   }
 }
 const mapStateToProps = state => ({
