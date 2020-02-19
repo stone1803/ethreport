0cc25d25f0339b17101e73877614e4af9b2dbdb3
Phuc Le <phucle@Phucs-MacBook-Pro.local>
2020-02-19 13:52:27 +0700

    50

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,7 +6,7 @@ export default class Menu extends Component {
       <div>
         <div className="logo">
           <a
-            href="http://www.creative-tim.com"
+            href="http://www.phucmap.com"
             className="simple-text logo-normal"
           >
             IP COIN ETH
@@ -29,31 +29,13 @@ export default class Menu extends Component {
             <li className="nav-item ">
               <a className="nav-link" href="./user.html">
                 <i className="material-icons">money</i>
-                <p>Thu Nhập</p>
+                <p>Thu Nhập Và Chi Phí</p>
               </a>
             </li>
             <li className="nav-item ">
               <a className="nav-link" href="./typography.html">
                 <i className="material-icons">library_books</i>
-                <p>Typography</p>
-              </a>
-            </li>
-            <li className="nav-item ">
-              <a className="nav-link" href="./icons.html">
-                <i className="material-icons">bubble_chart</i>
-                <p>Icons</p>
-              </a>
-            </li>
-            <li className="nav-item ">
-              <a className="nav-link" href="./map.html">
-                <i className="material-icons">location_ons</i>
-                <p>Maps</p>
-              </a>
-            </li>
-            <li className="nav-item ">
-              <a className="nav-link" href="./notifications.html">
-                <i className="material-icons">notifications</i>
-                <p>Notifications</p>
+                <p>Tiện Ích</p>
               </a>
             </li>
             {/* <li class="nav-item active-pro ">
